46fa7ab180374b034ff2f992007bca1df82fd9e0
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-02-18 12:04:38 +0200

    adds task 1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from 'date-fns';
+
 import {
   Card,
   CardBody,
@@ -11,9 +13,39 @@ import {
   UserBox,
   UserInfo,
   UserName,
-  Date,
+  Date as StyledDate,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+  name,
+  postedAt,
+}) => {
+  const result = formatDistanceToNow(new Date(postedAt));
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <StyledDate>{result}</StyledDate>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
