f6a907a466abe9fc5c9358d876bd1474421c07da
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-03-04 08:55:09 +0200

    save

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -4,10 +4,11 @@ import {
   StatisticCounter,
 } from './StatisticItem.styled';
 
-export const StatisticItem = ({ title, total }) => {
+export const StatisticItem = ({ title, total, icon: Icon }) => {
   return (
     <StatisticBox>
       {/* Тут має бути іконка */}
+      <Icon size={30} color={'red'} />
       <StatisticCounter>{total}</StatisticCounter>
       <StatisticText>{title}</StatisticText>
     </StatisticBox>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,21 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
+import PropTypes from 'prop-types';
+
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+
+const icons = {
+  1: FaRegThumbsUp,
+  2: MdPeople,
+  3: MdOutlineProductionQuantityLimits,
+  4: GiTreeDoor,
+  default: AiOutlineInfoCircle,
+};
+
 export const Statistics = ({ title, stats }) => {
   return (
     <>
-      {' '}
       {title && <StatisticTitle>{title}</StatisticTitle>}
       <StatisticsList>
         {stats.map(elem => {
@@ -16,10 +27,22 @@ export const Statistics = ({ title, stats }) => {
               title={elem.title}
               total={elem.total}
               key={elem.id}
+              icon={icons[elem.id] ?? icons.default}
             />
           );
         })}
       </StatisticsList>
     </>
   );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      total: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
 };
